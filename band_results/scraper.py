36f4f4373e91cfff03a6fc115c344d9010ad1a0c
Dominic Francis <dominicf09@gmail.com>
2024-02-12 22:01:27 +0000

    Scrape results page urls from contests page

--- band_results/scraper.py
+++ band_results/scraper.py
@@ -28,3 +28,28 @@ def get_results(url):
     results = list(map(get_data_from_row, rows))
 
     return results
+
+
+def get_urls(number=None):
+    url = "https://www.brassbandresults.co.uk/contests/national-finals-championship-section"
+
+    hdr = {"User-Agent": "Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
+    request = Request(url, headers=hdr)
+
+    page = urlopen(request)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    soup = BeautifulSoup(html, "html.parser")
+    contests_table = soup.main.tbody
+    events = contests_table.find_all("a", attrs={"class": "bbr-event"})
+    urls = ["https://www.brassbandresults.co.uk" + x.get("href") for x in events]
+
+    if number is None:
+        return urls
+
+    return urls[:number]
+
+
+if __name__ == "__main__":
+    print(get_urls())
