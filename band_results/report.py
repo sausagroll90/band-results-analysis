080177e7fa68a9e244bd71154365937a7f5040f8
Dominic Francis <dominicf09@gmail.com>
2024-04-05 22:39:45 +0100

    Add save_rows_to_file function

--- band_results/report.py
+++ band_results/report.py
@@ -23,8 +23,17 @@ def format_key_value_for_console(key, value):
     return formatted_key_value
 
 
-def save_rows_to_txt(rows):
-    with open("winners.txt", "w", encoding="utf-8") as f:
+def save_rows_to_file(file_path, rows):
+    if file_path.endswith(".txt"):
+        save_rows_to_txt(file_path, rows)
+    elif file_path.endswith(".csv"):
+        save_rows_to_csv(file_path, rows)
+    else:
+        raise ValueError("File extension must be .txt or .csv")
+
+
+def save_rows_to_txt(file_path, rows):
+    with open(file_path, "w", encoding="utf-8") as f:
         for row in rows:
             formatted_row = format_row_for_txt(row)
             f.write(formatted_row)
@@ -46,14 +55,21 @@ def format_key_value_for_txt(key, value):
     return formatted_row
 
 
-def save_rows_to_csv(rows):
-    with open("winners.csv", "w", newline="") as csvfile:
+def save_rows_to_csv(file_path, rows):
+    with open(file_path, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
-        header = rows[0].keys()
+        header = make_csv_header(rows)
         writer.writerow(header)
         for row in rows:
             formatted_row = format_row_for_csv(row)
             writer.writerow(formatted_row)
+
+
+def make_csv_header(rows):
+    header = rows[0].keys()
+    if "count(*)" in header:
+        header[header.index("count(*)")] = "count"
+    return header
 
 
 def format_row_for_csv(row):
@@ -63,7 +79,11 @@ def format_row_for_csv(row):
 
 def main():
     winners = get_winners()
-    save_rows_to_csv(winners)
+    wins_by_region = get_wins_by_region()
+    save_rows_to_file("winners.txt", winners)
+    save_rows_to_file("wins_by_region.txt", wins_by_region)
+    save_rows_to_file("winners.csv", winners)
+    save_rows_to_file("wins_by_region.csv", wins_by_region)
 
 
 if __name__ == "__main__":
