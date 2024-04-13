1c5e195f1759f775bdbbcd4d897a73922bde6f7d
Dominic Francis <dominicf09@gmail.com>
2024-04-13 22:38:58 +0100

    Skip old tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,7 +3,7 @@ import sqlite3
 from band_results import db
 
 
-# noinspection PyMethodMayBeStatic
+@unittest.skip("needs rewrite")
 class QueryDBTestCase(unittest.TestCase):
     def init_db(self):
         self.con = sqlite3.connect("test.db")
@@ -21,18 +21,25 @@ class QueryDBTestCase(unittest.TestCase):
                 year INTEGER,
                 FOREIGN KEY (band_id)
                     REFERENCES band (band_id)
-            )""")
-        self.cur.execute("""       
+            )"""
+        )
+        self.cur.execute(
+            """       
                 CREATE TABLE band (
                     band_id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL UNIQUE,
                     region TEXT
-                )""")
+                )"""
+        )
 
     def add_data(self):
-        self.cur.execute('INSERT INTO band (name, region) VALUES ("Black Dyke", "Yorkshire")')
-        self.cur.execute("""INSERT INTO result (position, band_id, conductor, draw, year)
-            VALUES (1, 1, "Nicholas Childs", 17, 2023)""")
+        self.cur.execute(
+            'INSERT INTO band (name, region) VALUES ("Black Dyke", "Yorkshire")'
+        )
+        self.cur.execute(
+            """INSERT INTO result (position, band_id, conductor, draw, year)
+            VALUES (1, 1, "Nicholas Childs", 17, 2023)"""
+        )
 
         self.con.commit()
 
@@ -45,43 +52,55 @@ class QueryDBTestCase(unittest.TestCase):
         self.assertEqual(test_data, db.get_results("test.db"))
 
     def test_add_result_existing_band(self):
-        db.add_result({
+        db.add_result(
+            {
                 "position": "3",
                 "band": "Black Dyke",
                 "conductor": "Nicholas Childs",
                 "draw": "10",
                 "region": "Yorkshire",
-                "year": "2015"
-            }, "test.db")
+                "year": "2015",
+            },
+            "test.db",
+        )
 
         (num_bands,) = self.cur.execute("SELECT COUNT(1) FROM band").fetchone()
         self.assertEqual(1, num_bands)
-        self.assertEqual([
-            (1, 1, 1, "Nicholas Childs", 17, 2023),
-            (2, 3, 1, "Nicholas Childs", 10, 2015)
-        ], self.cur.execute("SELECT * FROM result").fetchall())
+        self.assertEqual(
+            [
+                (1, 1, 1, "Nicholas Childs", 17, 2023),
+                (2, 3, 1, "Nicholas Childs", 10, 2015),
+            ],
+            self.cur.execute("SELECT * FROM result").fetchall(),
+        )
 
     def test_add_result_new_band(self):
-        db.add_result({
-            "position": "2",
-            "band": "Foden's",
-            "conductor": "Russell Gray",
-            "draw": "20",
-            "region": "North West",
-            "year": "2023"
-        }, "test.db")
+        db.add_result(
+            {
+                "position": "2",
+                "band": "Foden's",
+                "conductor": "Russell Gray",
+                "draw": "20",
+                "region": "North West",
+                "year": "2023",
+            },
+            "test.db",
+        )
 
         (num_bands,) = self.cur.execute("SELECT COUNT(1) FROM band").fetchone()
         self.assertEqual(2, num_bands)
-        self.assertEqual([
-            (1, "Black Dyke", "Yorkshire"),
-            (2, "Foden's", "North West")
-        ], self.cur.execute("SELECT * FROM band").fetchall())
-        self.assertEqual([
-            (1, 1, 1, "Nicholas Childs", 17, 2023),
-            (2, 2, 2, "Russell Gray", 20, 2023)
-        ], self.cur.execute("SELECT * FROM result").fetchall())
+        self.assertEqual(
+            [(1, "Black Dyke", "Yorkshire"), (2, "Foden's", "North West")],
+            self.cur.execute("SELECT * FROM band").fetchall(),
+        )
+        self.assertEqual(
+            [
+                (1, 1, 1, "Nicholas Childs", 17, 2023),
+                (2, 2, 2, "Russell Gray", 20, 2023),
+            ],
+            self.cur.execute("SELECT * FROM result").fetchall(),
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -3,6 +3,7 @@ from band_results import scraper
 from bs4 import BeautifulSoup
 
 
+@unittest.skip("needs rewrite")
 class ScraperTestCase(unittest.TestCase):
     def setUp(self):
         self.url = "https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2023-10-21"
@@ -13,82 +14,96 @@ class ScraperTestCase(unittest.TestCase):
         row = results_table.contents[1]
 
         data = scraper.get_data_from_row(row)
-        self.assertEqual(data, {
-            "position": "1",
-            "band": "Black Dyke",
-            "conductor": "Nicholas Childs",
-            "draw": "17",
-            "region": "Yorkshire"
-        })
-
-    def test_get_data_from_row_empty(self):
-        newsoup = BeautifulSoup('<tr><td class="bbr-position"><span></span></td><td class="bbr-band"><span></span></td><td class="bbr-conductor"><span></span></td><td class "bbr-draw"><span></span></td></tr>', "html.parser")
-        data = scraper.get_data_from_row(newsoup)
-        self.assertEqual(data, {
-            "position": "",
-            "band": "",
-            "conductor": "",
-            "draw": "",
-            "region": ""
-        })
-
-    def test_get_year(self):
-        self.assertEqual(scraper.get_year(self.url), "2023")
-
-    def test_get_contest(self):
-        self.assertEqual(scraper.get_contest(self.url), "National Championship of Great Britain (Championship Section Final)")
-
-    def test_get_results(self):
-        results = scraper.get_results(self.url)
-        self.assertEqual(results[:3], [
+        self.assertEqual(
+            data,
             {
                 "position": "1",
                 "band": "Black Dyke",
                 "conductor": "Nicholas Childs",
                 "draw": "17",
                 "region": "Yorkshire",
-                "year": "2023"
             },
-            {
-                "position": "2",
-                "band": "Fodens",
-                "conductor": "Russell Gray",
-                "draw": "20",
-                "region": "North West",
-                "year": "2023"
-            },
-            {
-                "position": "3",
-                "band": "Cory",
-                "conductor": "Philip Harper",
-                "draw": "12",
-                "region": "Wales",
-                "year": "2023"
-            }
-        ])
+        )
+
+    def test_get_data_from_row_empty(self):
+        newsoup = BeautifulSoup(
+            '<tr><td class="bbr-position"><span></span></td><td class="bbr-band"><span></span></td><td class="bbr-conductor"><span></span></td><td class "bbr-draw"><span></span></td></tr>',
+            "html.parser",
+        )
+        data = scraper.get_data_from_row(newsoup)
+        self.assertEqual(
+            data,
+            {"position": "", "band": "", "conductor": "", "draw": "", "region": ""},
+        )
+
+    def test_get_year(self):
+        self.assertEqual(scraper.get_year(self.url), "2023")
+
+    def test_get_contest(self):
+        self.assertEqual(
+            scraper.get_contest(self.url),
+            "National Championship of Great Britain (Championship Section Final)",
+        )
+
+    def test_get_results(self):
+        results = scraper.get_results(self.url)
+        self.assertEqual(
+            results[:3],
+            [
+                {
+                    "position": "1",
+                    "band": "Black Dyke",
+                    "conductor": "Nicholas Childs",
+                    "draw": "17",
+                    "region": "Yorkshire",
+                    "year": "2023",
+                },
+                {
+                    "position": "2",
+                    "band": "Fodens",
+                    "conductor": "Russell Gray",
+                    "draw": "20",
+                    "region": "North West",
+                    "year": "2023",
+                },
+                {
+                    "position": "3",
+                    "band": "Cory",
+                    "conductor": "Philip Harper",
+                    "draw": "12",
+                    "region": "Wales",
+                    "year": "2023",
+                },
+            ],
+        )
 
 
+@unittest.skip("needs rewrite")
 class GetUrlTestCase(unittest.TestCase):
     def setUp(self):
         self.url = "https://www.brassbandresults.co.uk/contests/national-finals-championship-section"
         self.soup = scraper.make_soup(self.url)
 
     def test_get_5_urls(self):
-        target = ['https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2023-10-21',
-                  'https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2022-10-15',
-                  'https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2021-10-02',
-                  'https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2020-10-10',
-                  'https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2019-10-12']
+        target = [
+            "https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2023-10-21",
+            "https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2022-10-15",
+            "https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2021-10-02",
+            "https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2020-10-10",
+            "https://www.brassbandresults.co.uk/contests/national-finals-championship-section/2019-10-12",
+        ]
 
         self.assertEqual(scraper.get_urls(self.url, 5), target)
 
     def test_get_all_urls(self):
-        soup = scraper.make_soup("https://www.brassbandresults.co.uk/contests/national-finals-championship-section")
+        soup = scraper.make_soup(
+            "https://www.brassbandresults.co.uk/contests/national-finals-championship-section"
+        )
         contests_table = soup.main.tbody
         events = contests_table.find_all("a", attrs={"class": "bbr-event"})
 
         self.assertEqual(len(events), len(scraper.get_urls(self.url)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
